211b82ac456310d343e882eff9601ea6105eda90
John <johnbinek4@gmail.com>
2025-03-26 22:10:09 -0400

    quick best practices

---
+++ practiceCpp/BestPractices.cpp
@@ -0,0 +1,20 @@
+// Best Practices
+
+// Indentation: 4 spaces or tab
+
+// Compiler ignores whitespace and comments which are used to read code it is near
+
+/*
+Use
+this
+for
+multiline
+*/
+
+// https://isocpp.github.io/CppCoreGuidelines/CppCoreGuidelines
+
+#include <iostream>
+
+int main(){
+    return 0;
+}
