a6ce42b4fd8f3a8a6768bd47da8241119b6f6b70
John <johnbinek4@gmail.com>
2025-03-27 20:37:53 -0400

    Function Intro

---
+++ practiceCpp/FunctionsIntro.cpp
@@ -0,0 +1,29 @@
+#include <>
+
+// a lot are already made, so no need to remake. 
+// Data in () is known as argument, and often we need multiple
+
+// ; at end? will depend. 
+
+// may assign, x = funct(x,x,x); then call
+
+// creating fucntions
+
+/*
+When to do so? to complete task that is repetitive.
+The name of a function is known as an identifier.
+*/
+
+// this is the main function, it is called automatically when exceuted. 
+// input goes in (), arguments go into parameters
+
+// this is function definition
+int multiply(int x, int y){  
+    return (x*y); 
+}
+
+int x = 5;
+
+multiply(x, 2); // this is the calling
+
+// declaration is done automatically. The code needs to be set above where the code is called. 
